bedd69f321f32d33feeff274302f08cac987706c
Alexander <aovoronin.piano@gmail.com>
2023-07-06 15:35:47 +0300

    init: initial configuration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { WebSocketServer } from "ws";
+import { SOME_CONST } from "./some-module/some-module.js";
 
 const websocketServer = new WebSocketServer({ port: 3000 });
 
 websocketServer.on("connection", function connection(ws) {
-  console.log("Start things");
+  console.log(SOME_CONST(), 'The server is started!');
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
